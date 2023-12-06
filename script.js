195567af42b9ecd42bd7d7f50e8d014bba73cd11
Jiabao Ding <jiabaoding6@gmail.com>
2023-12-06 14:34:09 -0500

    change style of response and add comment

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var backNumber = [];
 var countNumber = [];
 var allNumbers = [];
 
+// check if the game is still on the page of rule declaration or already started
+
 if (!localStorage.getItem("stateOfBegin")) {
   localStorage.setItem("stateOfBegin", "false");
 }
@@ -14,6 +16,7 @@ function ready() {
   localStorage.setItem("stateOfBegin", "true");
 }
 
+// Game is started, the text of game rule will be hidden and the main page of the game shows on
 $(window).on("load", function () {
   if (stateOfBegin === "false") {
     $(".buttonBegin").on("click", function () {
@@ -26,7 +29,7 @@ $(window).on("load", function () {
     $(".game").css("display", "block");
   }
 
-  // Function to get a random number between 1 and 13
+  // Function to get a random number between 1 and 13, because the game were originally based on card, the number the player will get is based on the number of card too
   function getRandomNumber() {
     return Math.ceil(Math.random() * 13);
   }
@@ -61,7 +64,7 @@ $(window).on("load", function () {
     allNumbers.push(numberToShow);
   }
 
-  // Function to check if the last character in resulting is not a number
+  // Function to check if the last character in id of resulting is not a number and not the operator of )
   function isLastCharacterNotNumber() {
     var lastCharacter = $("#resulting").text().split("");
     return (
@@ -70,13 +73,13 @@ $(window).on("load", function () {
     );
   }
 
-  // Set up styles for initial numbers
+  // Set up styles and values for initial numbers
   $(".numbers").each(function () {
     setNumberStyles($(this));
 
     var active = true;
 
-    // Click event for numbers
+    // Click event for numbers, every number will be used only once and after being used, the circle of this number will turn faint
     $(this).on("click", function () {
       console.log(allNumbers);
       if (active && isLastCharacterNotNumber()) {
@@ -89,7 +92,7 @@ $(window).on("load", function () {
     });
   });
 
-  // Click event for operators
+  // Click event for operators and the operator will only be added after a number
   $(".operator").each(function () {
     $(this).on("click", function () {
       var lastCharacter = $("#resulting").text().split("");
@@ -131,7 +134,7 @@ $(window).on("load", function () {
     });
   });
 
-  // Click event for back button
+  // Click event for back button. The back button returned the used number from resulting box back and let player have chance to use them again.
   $(".back").on("click", function () {
     var info = $("#resulting").text().split(" ");
     if (info.length > 0) {
@@ -155,7 +158,7 @@ $(window).on("load", function () {
     $("#resulting").text(info.join(" "));
   });
 
-  // Click event for numbers after back button is clicked
+  // Click event for numbers after back button is clicked. It makes sure that the returned number will only be returned once and reused once.
   $(".numbers").each(function () {
     $(this).on("click", function () {
       var lastCharacter = $("#resulting").text().split("").pop();
@@ -174,6 +177,7 @@ $(window).on("load", function () {
   //compare the result with number 24
   $(".go").on("click", function () {
     const originalText = $("#resulting").text();
+
     if (countNumber.length !== 4) {
       $("#resulting").text("All provided number should be used once");
       $("#resulting").css("fontSize", "20px");
@@ -202,6 +206,9 @@ $(window).on("load", function () {
       }
     }
   });
+
+  //Through using refresh button, the provided numbers will be regenerated
+
   $(".refresh").on("click", function () {
     location.reload();
   });
@@ -213,29 +220,9 @@ $(window).on("load", function () {
     location.reload();
     localStorage.removeItem("stateOfBegin");
   });
-
-  // if ($(".end").css("display") === "none") {
-  //   // Attach hover events to #target-paragraph
-  //   $("#target-paragraph").hover(
-  //     function () {
-  //       // Check the condition again before hiding .game
-  //       if ($(".end").css("display") === "none") {
-  //         $(".game").css("display", "none");
-  //         $(".explaination").fadeIn(1000);
-  //       }
-  //     },
-  //     function () {
-  //       // Check the condition again before showing .game
-  //       if ($(".end").css("display") === "none") {
-  //         setTimeout(function () {
-  //           $(".game").css("display", "block");
-  //         }, 1000);
-  //         $(".explaination").fadeOut(500);
-  //       }
-  //     }
-  //   );
-  // }
 });
+
+// Through Api the potential solution of using the 4 numbers to achieve 24 will be responsed.
 
 $(".sendToAPI").on("click", function () {
   // Assuming the API endpoint is 'https://helloacm.com/api/24/'
@@ -262,9 +249,15 @@ $(".sendToAPI").on("click", function () {
       // Handle the response data here
       console.log("API Response:", data);
       if (data.result.length > 1) {
-        alert("The Answer is:" + data.result[0]);
+        $("#clue").fadeIn(4000);
+        $("#clue").text("The Answer is: " + data.result[0]);
+        $("#clue").fadeOut(4000);
       } else {
-        alert("It is impossible with these 4 numbers, please refresh");
+        $("#clue").fadeIn(4000);
+        $("#clue").text(
+          "It is impossible with these 4 numbers, please refresh"
+        );
+        $("#clue").fadeOut(4000);
       }
     })
     .catch((error) => {
